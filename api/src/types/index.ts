b3fea73df1f608c976f788bf99dcab5a89e6839a
olabrador <labradoruseche@gmail.com>
2024-06-11 15:10:05 -0400

    chore(api-types): add base types for orders, transactions and matched orders

---
+++ api/src/types/index.ts
@@ -0,0 +1,23 @@
+export type Order = {
+  id: string;
+  type: string;
+  customerName: string;
+  date: string;
+  product: string;
+  price: number;
+};
+
+export type Transaction = {
+  id: string;
+  type: string;
+  customerName: string;
+  orderId: string;
+  date: string;
+  product: string;
+  price: number;
+  transactionType: string;
+  transactionDate: string;
+  transactionAmount: number;
+};
+
+export type MatchedRecord = Order & { transactions: Transaction[] };
